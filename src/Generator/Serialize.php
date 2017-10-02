f600f63f5fb7e7301121792dbeac0896fc80c2d4
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-10-03 01:04:18 +0200

    add serialize and template generator

---
+++ src/Generator/Serialize.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Api\Generator;
+
+use PSX\Api\GeneratorInterface;
+use PSX\Api\Resource;
+
+/**
+ * Serialize
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class Serialize implements GeneratorInterface
+{
+    /**
+     * @param \PSX\Api\Resource $resource
+     * @return string
+     */
+    public function generate(Resource $resource)
+    {
+        return serialize($resource);
+    }
+}
