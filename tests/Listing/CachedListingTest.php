4a30dfed4d67199e521da86af41af10b6271fbba
Christoph Kappestein <christoph.kappestein@gmail.com>
2017-10-03 12:22:32 +0200

    add cached and memory test listing

---
+++ tests/Listing/CachedListingTest.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Api\Tests\Listing;
+
+use Doctrine\Common\Cache\ArrayCache;
+use PSX\Api\ApiManager;
+use PSX\Api\Listing\CachedListing;
+use PSX\Api\Listing\MemoryListing;
+use PSX\Api\Tests\Parser\Annotation\TestController;
+use PSX\Cache\Pool;
+use PSX\Schema\SchemaManager;
+
+/**
+ * CachedListingTest
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+class CachedListingTest extends ListingTestCase
+{
+    protected function newListing()
+    {
+        $schemaReader = new \Doctrine\Common\Annotations\SimpleAnnotationReader();
+        $schemaReader->addNamespace('PSX\\Schema\\Parser\\Popo\\Annotation');
+
+        $apiReader = new \Doctrine\Common\Annotations\SimpleAnnotationReader();
+        $apiReader->addNamespace('PSX\\Api\\Annotation');
+
+        $apiManager = new ApiManager($apiReader, new SchemaManager($schemaReader));
+
+        $listing = new MemoryListing();
+        $listing->addResource($apiManager->getApi(TestController::class, '/foo'));
+
+        $cache = new Pool(new ArrayCache());
+
+        return new CachedListing($listing, $cache);
+    }
+}
---
+++ tests/Listing/ListingTestCase.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * PSX is a open source PHP framework to develop RESTful APIs.
+ * For the current version and informations visit <http://phpsx.org>
+ *
+ * Copyright 2010-2017 Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace PSX\Api\Tests\Listing;
+
+use PSX\Api\Resource;
+
+/**
+ * ListingTestCase
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    http://phpsx.org
+ */
+abstract class ListingTestCase extends \PHPUnit_Framework_TestCase
+{
+    public function testGetResourceIndex()
+    {
+        $listing   = $this->newListing();
+        $resources = $listing->getResourceIndex();
+
+        $this->assertEquals(1, count($resources));
+        $this->assertInstanceOf(Resource::class, $resources[0]);
+
+        $resources = $listing->getResourceIndex();
+
+        $this->assertEquals(1, count($resources));
+        $this->assertInstanceOf(Resource::class, $resources[0]);
+    }
+
+    public function testGetResource()
+    {
+        $listing  = $this->newListing();
+        $resource = $listing->getResource('/foo');
+
+        $this->assertInstanceOf(Resource::class, $resource);
+
+        $resource = $listing->getResource('/foo');
+
+        $this->assertInstanceOf(Resource::class, $resource);
+    }
+
+    /**
+     * @return \PSX\Api\ListingInterface
+     */
+    abstract protected function newListing();
+}
