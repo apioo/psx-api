4ea1de538844d7edd45845472e804ec3ef4cbfec
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-07-25 20:53:24 +0200

    use builder to create resources

--- tests/Generator/GeneratorTestCase.php
+++ tests/Generator/GeneratorTestCase.php
@@ -20,17 +20,10 @@
 
 namespace PSX\Api\Tests\Generator;
 
-use Doctrine\Common\Annotations\SimpleAnnotationReader;
-use PHPUnit\Framework\TestCase;
 use PSX\Api\Resource;
-use PSX\Api\ResourceCollection;
-use PSX\Api\Specification;
 use PSX\Api\SpecificationInterface;
-use PSX\Schema\Builder;
-use PSX\Schema\Definitions;
-use PSX\Schema\DefinitionsInterface;
+use PSX\Api\Tests\ApiManagerTestCase;
 use PSX\Schema\Generator\Code\Chunks;
-use PSX\Schema\SchemaManager;
 
 /**
  * GeneratorTestCase
@@ -39,215 +32,118 @@ use PSX\Schema\SchemaManager;
  * @license http://www.apache.org/licenses/LICENSE-2.0
  * @link    http://phpsx.org
  */
-abstract class GeneratorTestCase extends TestCase
+abstract class GeneratorTestCase extends ApiManagerTestCase
 {
     protected function getSpecification(): SpecificationInterface
     {
-        return Specification::fromResource(
-            $this->newResource(),
-            $this->newDefinitions()
-        );
-    }
+        $builder = $this->apiManager->getBuilder(Resource::STATUS_ACTIVE, '/foo/:name/:type');
 
-    private function newResource(): Resource
-    {
-        $resource = new Resource(Resource::STATUS_ACTIVE, '/foo/:name/:type');
-        $resource->setTitle('foo');
-        $resource->setDescription('lorem ipsum');
-        $resource->setPathParameters('Path');
-
-        $resource->addMethod(Resource\Factory::getMethod('GET')
-            ->setDescription('Returns a collection')
-            ->setOperationId('list.foo')
-            ->setQueryParameters('GetQuery')
-            ->addResponse(200, 'EntryCollection'));
-
-        $resource->addMethod(Resource\Factory::getMethod('POST')
-            ->setOperationId('create.foo')
-            ->setRequest('EntryCreate')
-            ->addResponse(201, 'EntryMessage'));
-
-        $resource->addMethod(Resource\Factory::getMethod('PUT')
-            ->setRequest('EntryUpdate')
-            ->addResponse(200, 'EntryMessage'));
-
-        $resource->addMethod(Resource\Factory::getMethod('DELETE')
-            ->setRequest('EntryDelete')
-            ->addResponse(200, 'EntryMessage'));
-
-        $resource->addMethod(Resource\Factory::getMethod('PATCH')
-            ->setRequest('EntryPatch')
-            ->addResponse(200, 'EntryMessage'));
-
-        return $resource;
-    }
-
-    private function newDefinitions(): DefinitionsInterface
-    {
-        $reader = new SimpleAnnotationReader();
-        $reader->addNamespace('PSX\\Api\\Annotation');
-
-        $schemaManager = new SchemaManager($reader);
-
-        $builder = new Builder();
-        $builder->addString('name')
+        $builder->setTitle('foo');
+        $builder->setDescription('lorem ipsum');
+        $path = $builder->setPathParameters('Path');
+        $path->addString('name')
             ->setDescription('Name parameter')
             ->setMinLength(0)
             ->setMaxLength(16)
             ->setPattern('[A-z]+');
-        $builder->addString('type')
+        $path->addString('type')
             ->setEnum(['foo', 'bar']);
-        $builder->setRequired(['name']);
-        $path = $builder->getType();
+        $path->setRequired(['name']);
 
-        $builder = new Builder();
-        $builder->addInteger('startIndex')
+        $get = $builder->addMethod('GET');
+        $get->setDescription('Returns a collection');
+        $get->setOperationId('list.foo');
+        $query = $get->setQueryParameters('GetQuery');
+        $query->addInteger('startIndex')
             ->setDescription('startIndex parameter')
             ->setMinimum(0)
             ->setMaximum(32);
-        $builder->addNumber('float');
-        $builder->addBoolean('boolean');
-        $builder->addDate('date');
-        $builder->addDateTime('datetime');
-        $builder->setRequired(['startIndex']);
-        $getQuery = $builder->getType();
+        $query->addNumber('float');
+        $query->addBoolean('boolean');
+        $query->addDate('date');
+        $query->addDateTime('datetime');
+        $query->setRequired(['startIndex']);
+        $get->addResponse(200, Schema\Collection::class);
 
-        $definitions = new Definitions();
-        $definitions->addType('Path', $path);
-        $definitions->addType('GetQuery', $getQuery);
-        $definitions->merge($schemaManager->getSchema(Schema\Collection::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Create::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Update::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Delete::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Patch::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Message::class)->getDefinitions());
+        $post = $builder->addMethod('POST');
+        $post->setOperationId('create.foo');
+        $post->setRequest(Schema\Create::class);
+        $post->addResponse(201, Schema\Message::class);
 
-        return $definitions;
+        $put = $builder->addMethod('PUT');
+        $put->setRequest(Schema\Update::class);
+        $put->addResponse(200, Schema\Message::class);
+
+        $delete = $builder->addMethod('DELETE');
+        $delete->setRequest(Schema\Delete::class);
+        $delete->addResponse(200, Schema\Message::class);
+
+        $patch = $builder->addMethod('PATCH');
+        $patch->setRequest(Schema\Patch::class);
+        $patch->addResponse(200, Schema\Message::class);
+
+        return $builder->getSpecification();
     }
 
     protected function getSpecificationCollection(): SpecificationInterface
     {
-        return new Specification(
-            $this->newResourceCollection(),
-            $this->newDefinitionsCollection()
-        );
-    }
+        $builder = $this->apiManager->getBuilder(Resource::STATUS_ACTIVE, '/foo');
+        $builder->setTitle('foo');
 
-    private function newResourceCollection()
-    {
-        $collection = new ResourceCollection();
+        $get = $builder->addMethod('GET');
+        $get->setDescription('Returns a collection');
+        $get->addResponse(200, Schema\Collection::class);
 
-        $resource = new Resource(Resource::STATUS_ACTIVE, '/foo');
-        $resource->setTitle('foo');
+        $post = $builder->addMethod('POST');
+        $post->setRequest(Schema\Create::class);
+        $post->addResponse(201, Schema\Message::class);
 
-        $resource->addMethod(Resource\Factory::getMethod('GET')
-            ->setDescription('Returns a collection')
-            ->addResponse(200, 'EntryCollection'));
+        $builder = $builder->addResource(Resource::STATUS_ACTIVE, '/bar/:foo');
+        $builder->setTitle('bar');
+        $builder->setPathParameters('PathFoo')->addString('foo');
 
-        $resource->addMethod(Resource\Factory::getMethod('POST')
-            ->setRequest('EntryCreate')
-            ->addResponse(201, 'EntryMessage'));
+        $get = $builder->addMethod('GET');
+        $get->setDescription('Returns a collection');
+        $get->addResponse(200, Schema\Collection::class);
 
-        $collection->set($resource);
+        $post = $builder->addMethod('POST');
+        $post->setRequest(Schema\Create::class);
+        $post->addResponse(201, Schema\Message::class);
 
-        $resource = new Resource(Resource::STATUS_ACTIVE, '/bar/:foo');
-        $resource->setTitle('bar');
-        $resource->setPathParameters('PathFoo');
-        $resource->addMethod(Resource\Factory::getMethod('GET')
-            ->setDescription('Returns a collection')
-            ->addResponse(200, 'EntryCollection'));
+        $builder = $builder->addResource(Resource::STATUS_ACTIVE, '/bar/$year<[0-9]+>');
+        $builder->setTitle('bar');
+        $builder->setPathParameters('PathYear')->addString('year');
 
-        $resource->addMethod(Resource\Factory::getMethod('POST')
-            ->setRequest('EntryCreate')
-            ->addResponse(201, 'EntryMessage'));
+        $get = $builder->addMethod('GET');
+        $get->setDescription('Returns a collection');
+        $get->addResponse(200, Schema\Collection::class);
 
-        $collection->set($resource);
+        $post = $builder->addMethod('POST');
+        $post->setRequest(Schema\Create::class);
+        $post->addResponse(201, Schema\Message::class);
 
-        $resource = new Resource(Resource::STATUS_ACTIVE, '/bar/$year<[0-9]+>');
-        $resource->setTitle('bar');
-        $resource->setPathParameters('PathYear');
-        $resource->addMethod(Resource\Factory::getMethod('GET')
-            ->setDescription('Returns a collection')
-            ->addResponse(200, 'EntryCollection'));
-
-        $resource->addMethod(Resource\Factory::getMethod('POST')
-            ->setRequest('EntryCreate')
-            ->addResponse(201, 'EntryMessage'));
-
-        $collection->set($resource);
-
-        return $collection;
-    }
-
-    private function newDefinitionsCollection(): DefinitionsInterface
-    {
-        $reader = new SimpleAnnotationReader();
-        $reader->addNamespace('PSX\\Api\\Annotation');
-
-        $schemaManager = new SchemaManager($reader);
-
-        $builder = new Builder();
-        $builder->addString('foo');
-        $pathFoo = $builder->getType();
-
-        $builder = new Builder();
-        $builder->addString('year');
-        $pathYear = $builder->getType();
-
-        $definitions = new Definitions();
-        $definitions->addType('PathFoo', $pathFoo);
-        $definitions->addType('PathYear', $pathYear);
-        $definitions->merge($schemaManager->getSchema(Schema\Collection::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Create::class)->getDefinitions());
-        $definitions->merge($schemaManager->getSchema(Schema\Message::class)->getDefinitions());
-
-        return $definitions;
+        return $builder->getSpecification();
     }
 
     protected function getSpecificationComplex(): SpecificationInterface
     {
-        return Specification::fromResource(
-            $this->newResourceComplex(),
-            $this->newDefinitionsComplex()
-        );
-    }
+        $builder = $this->apiManager->getBuilder(Resource::STATUS_ACTIVE, '/foo/:name/:type');
+        $builder->setTitle('foo');
+        $builder->setDescription('lorem ipsum');
+        $path = $builder->setPathParameters('Path');
+        $path->addString('name');
+        $path->addString('type');
+        $path->setRequired(['name', 'type']);
 
-    private function newResourceComplex(): Resource
-    {
-        $resource = new Resource(Resource::STATUS_ACTIVE, '/foo/:name/:type');
-        $resource->setTitle('foo');
-        $resource->setDescription('lorem ipsum');
-        $resource->setPathParameters('Path');
+        $post = $builder->addMethod('POST');
+        $post->setDescription('Returns a collection');
+        $post->setOperationId('postEntryOrMessage');
+        $post->setTags(['foo']);
+        $post->setRequest(Schema\Complex::class);
+        $post->addResponse(200, Schema\Complex::class);
+        $post->setSecurity('OAuth2', ['foo']);
 
-        $resource->addMethod(Resource\Factory::getMethod('POST')
-            ->setDescription('Returns a collection')
-            ->setOperationId('postEntryOrMessage')
-            ->setTags(['foo'])
-            ->setRequest('EntryOrMessage')
-            ->addResponse(200, 'EntryOrMessage')
-            ->setSecurity('OAuth2', ['foo']));
-
-        return $resource;
-    }
-
-    private function newDefinitionsComplex(): DefinitionsInterface
-    {
-        $reader = new SimpleAnnotationReader();
-        $reader->addNamespace('PSX\\Api\\Annotation');
-
-        $schemaManager = new SchemaManager($reader);
-
-        $builder = new Builder();
-        $builder->addString('name');
-        $builder->addString('type');
-        $builder->setRequired(['name', 'type']);
-        $path = $builder->getType();
-
-        $definitions = new Definitions();
-        $definitions->addType('Path', $path);
-        $definitions->merge($schemaManager->getSchema(Schema\Complex::class)->getDefinitions());
-
-        return $definitions;
+        return $builder->getSpecification();
     }
 
     protected function getPaths()
