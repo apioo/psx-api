8a373c36cd45916fa32cf86d135d46b608e20f4b
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-03-16 20:46:51 +0100

    path target class or method

--- src/Attribute/Path.php
+++ src/Attribute/Path.php
@@ -29,7 +29,7 @@ use Attribute;
  * @license http://www.apache.org/licenses/LICENSE-2.0
  * @link    https://phpsx.org
  */
-#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_PROPERTY)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
 class Path
 {
     public function __construct(public string $path)
