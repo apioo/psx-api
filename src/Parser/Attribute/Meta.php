71b456eb9d645f5154197a97a97c71d11fcec0c3
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-18 02:36:55 +0200

    handle parameter attributes

--- src/Parser/Attribute/Meta.php
+++ src/Parser/Attribute/Meta.php
@@ -32,6 +32,7 @@ use PSX\Api\Attribute\Outgoing;
 use PSX\Api\Attribute\Path;
 use PSX\Api\Attribute\PathParam;
 use PSX\Api\Attribute\QueryParam;
+use PSX\Api\Attribute\ResponseStatus;
 use PSX\Api\Attribute\Security;
 use PSX\Api\Attribute\Tags;
 
@@ -70,6 +71,7 @@ class Meta
     private ?Security $security = null;
     private ?Deprecated $deprecated = null;
     private ?Authorization $authorization = null;
+    private ?ResponseStatus $responseStatus = null;
 
     public function __construct(array $attributes)
     {
@@ -102,25 +104,27 @@ class Meta
                 $this->deprecated = $attribute;
             } elseif ($attribute instanceof Authorization) {
                 $this->authorization = $attribute;
+            } elseif ($attribute instanceof ResponseStatus) {
+                $this->responseStatus = $attribute;
             }
         }
     }
 
     public function merge(Meta $meta)
     {
-        if ($this->exclude == null) {
+        if ($this->exclude === null) {
             $this->exclude = $meta->getExclude();
         }
 
-        if ($this->description == null) {
+        if ($this->description === null) {
             $this->description = $meta->getDescription();
         }
 
-        if ($this->method == null) {
+        if ($this->method === null) {
             $this->method = $meta->getMethod();
         }
 
-        if ($this->path == null) {
+        if ($this->path === null) {
             $this->path = $meta->getPath();
         }
 
@@ -128,22 +132,34 @@ class Meta
         $this->headerParams = array_merge($this->headerParams, $meta->getHeaderParams());
         $this->queryParams = array_merge($this->queryParams, $meta->getQueryParams());
 
-        if ($this->incoming == null) {
+        if ($this->incoming === null) {
             $this->incoming = $meta->getIncoming();
         }
 
         $this->outgoing = array_merge($this->outgoing, $meta->getOutgoing());
 
-        if ($this->operationId == null) {
+        if ($this->operationId === null) {
             $this->operationId = $meta->getOperationId();
         }
 
-        if ($this->tags == null) {
+        if ($this->tags === null) {
             $this->tags = $meta->getTags();
         }
 
-        if ($this->security == null) {
+        if ($this->security === null) {
             $this->security = $meta->getSecurity();
+        }
+
+        if ($this->deprecated === null) {
+            $this->deprecated = $meta->getDeprecated();
+        }
+
+        if ($this->authorization === null) {
+            $this->authorization = $meta->getAuthorization();
+        }
+
+        if ($this->responseStatus === null) {
+            $this->responseStatus = $meta->getResponseStatus();
         }
     }
 
@@ -260,6 +276,11 @@ class Meta
     public function getAuthorization(): ?Authorization
     {
         return $this->authorization;
+    }
+
+    public function getResponseStatus(): ?ResponseStatus
+    {
+        return $this->responseStatus;
     }
 
     public function isExcluded(): bool
