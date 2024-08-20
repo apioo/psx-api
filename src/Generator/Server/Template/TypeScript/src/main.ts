13c9f7b65f602b5c9f75d9e31a7513eac53c72b5
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-20 21:55:23 +0200

    fix import

--- src/Generator/Server/Template/TypeScript/src/main.ts
+++ src/Generator/Server/Template/TypeScript/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.ts.twig';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
